8c0afd59bf3c98f23986b65abf6db910c743d4d6
emiko <31835314+siroemk@users.noreply.github.com>
2022-02-21 16:05:21 +0900

    todoデータにidを追加した

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
 
 const Storage = {
   fetch() {
-    return JSON.parse(localStorage.getItem('todoLists'))
+    return JSON.parse(localStorage.getItem('todoLists')) || []
   }
 }
 
 const app = new Vue({
   el: '#app',
   data: {
-    todoLists: Storage.fetch()
+    todoLists: Storage.fetch(),
+    text: ''
   },
   methods: {
     addText: function() {
       this.todoLists.push({
+        index: new Date().getTime().toString(),
         text: this.text,
         finished: false
       })
