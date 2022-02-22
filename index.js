1cf60adb472dc42cc0ac1d78276c02a8e8e034b8
emiko <31835314+siroemk@users.noreply.github.com>
2022-02-22 17:46:26 +0900

    todoを編集できるようにした

--- index.js
+++ index.js
@@ -9,7 +9,8 @@ const app = new Vue({
   el: '#app',
   data: {
     todoLists: Storage.fetch(),
-    text: ''
+    text: '',
+    editIndex: null
   },
   methods: {
     addText: function() {
@@ -24,6 +25,16 @@ const app = new Vue({
       const index = this.todoLists.indexOf(todo)
       this.todoLists.splice(index, 1)
       localStorage.setItem('todoLists', JSON.stringify(this.todoLists))
+    },
+    editText: function(todo) {
+      this.editIndex = todo.index
+    },
+    updateText: function(todo) {
+      const index = this.todoLists.indexOf(todo)
+      const editTodo = this.todoLists[index]
+      editTodo['text'] = todo.text
+      localStorage.setItem('todoLists', JSON.stringify(this.todoLists))
+      this.editIndex = ''
     }
   }
 })
