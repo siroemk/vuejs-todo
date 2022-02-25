61007aa3f58bfb2cf001e934905e1121616a68dd
emiko <31835314+siroemk@users.noreply.github.com>
2022-02-25 16:17:27 +0900

    saveをStorage側に移した

--- index.js
+++ index.js
@@ -2,6 +2,9 @@
 const Storage = {
   fetch() {
     return JSON.parse(localStorage.getItem('todoLists')) || []
+  },
+  save(todoLists) {
+    localStorage.setItem('todoLists', JSON.stringify(todoLists))
   }
 }
 
@@ -14,27 +17,30 @@ const app = new Vue({
   },
   methods: {
     addText: function() {
+      if(this.text === '') {
+        alert("ToDoを入力してください")
+        return
+      }
       this.todoLists.push({
         index: new Date().getTime().toString(),
         text: this.text,
         finished: false
       })
-      this.save(this.todoLists)
+      Storage.save(this.todoLists)
       this.text = ''
-    },
-    deleteText: function(todo) {
-      const index = this.todoLists.indexOf(todo)
-      this.todoLists.splice(index, 1)
-      this.save(this.todoLists)
     },
     editText: function(todo) {
       this.currentIndex = todo.index
     },
     updateText: function(todo) {
+      if(todo.text === '') {
+        alert("ToDoを入力してください")
+        return
+      }
       const index = this.todoLists.indexOf(todo)
       const editTodo = this.todoLists[index]
       editTodo['text'] = todo.text
-      localStorage.setItem('todoLists', JSON.stringify(this.todoLists))
+      Storage.save(this.todoLists)
       this.currentIndex = ''
     },
     cancelText: function() {
@@ -44,10 +50,12 @@ const app = new Vue({
       const index = this.todoLists.indexOf(todo)
       const Todo = this.todoLists[index]
       Todo.finished = !Todo.finished
-      this.save(this.todoLists)
+      Storage.save(this.todoLists)
     },
-    save: function(todoLists) {
-      localStorage.setItem('todoLists', JSON.stringify(todoLists))
+    deleteText: function(todo) {
+      const index = this.todoLists.indexOf(todo)
+      this.todoLists.splice(index, 1)
+      Storage.save(this.todoLists)
     }
   }
 })
